1616b953d3b41b29e2281420137169b631c0ca6c
stackops <stackops@snn-test-net-2.novalocal>
2022-01-31 02:28:51 +0700

    Fix call telegramush

--- vars/k8sRollbackBeta.groovy
+++ vars/k8sRollbackBeta.groovy
@@ -1,5 +1,5 @@
 def call() {
     slackSend(botUser: true, channel: 'sang-test', tokenCredentialId: 'slack-token', color: '#ff0000', message: 'Test error build: ' + env.TAG_BETA)
     sendTelegram(env.JOB_BASE_NAME + ' error build: ' + env.TAG_BETA)
-    error "Failed staging tests
+    error "Failed staging tests"
 }
