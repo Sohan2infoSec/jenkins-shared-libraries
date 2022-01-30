8b3d81d20ea34370d1f55af4a30396f2b933fd5a
stackops <stackops@snn-test-net-2.novalocal>
2022-01-31 02:07:39 +0700

    Fix call telegram

--- vars/k8sRollback.groovy
+++ vars/k8sRollback.groovy
@@ -2,6 +2,6 @@ def call(project) {
     sh """helm rollback \
         ${project} 0 \
         --namespace ${project}"""
-    sendTelegram(env.JOB_BASE_NAME + ' error build.')
+    sendTelegram(env.JOB_BASE_NAME + ' error deploy.')
     error "Failed production tests"
 }
