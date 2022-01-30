c424f2553cd808f9c965288cd58441bd7596bf88
stackops <stackops@snn-test-net-2.novalocal>
2022-01-31 02:22:21 +0700

    Fix call telegram

--- vars/k8sFuncTestGolang.groovy
+++ vars/k8sFuncTestGolang.groovy
@@ -1,6 +1,6 @@
 def call(project, addr) {
     sh "go get -d -v -t"
-    sh """ADDRESS=${addr} \
+    sh """DURATION=1 ADDRESS=${addr} \
         go test ./... -v \
         --run FunctionalTest"""
 }
