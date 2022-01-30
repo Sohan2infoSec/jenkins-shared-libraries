b2e002b9e136bc8c1c12cac89e7b5671da050bde
stackops <stackops@snn-test-net-2.novalocal>
2022-01-31 02:38:06 +0700

    De-couple rollback and telegram && remove RollbackBeta

--- vars/k8sRollback.groovy
+++ vars/k8sRollback.groovy
@@ -2,6 +2,5 @@ def call(project) {
     sh """helm rollback \
         ${project} 0 \
         --namespace ${project}"""
-    sendTelegram(env.JOB_BASE_NAME + ' error deploy.')
     error "Failed production tests"
 }
