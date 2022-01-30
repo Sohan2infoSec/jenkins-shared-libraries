ba2e104ae1dfc3da9c36694426e5757d4832a3b8
stackops <stackops@snn-test-net-2.novalocal>
2022-01-31 01:51:32 +0700

    add telegram alert

--- vars/k8sRollback.groovy
+++ vars/k8sRollback.groovy
@@ -2,5 +2,6 @@ def call(project) {
     sh """helm rollback \
         ${project} 0 \
         --namespace ${project}"""
+    sendTelegram(env.JOB_BASE_NAME + ' error build.')
     error "Failed production tests"
 }
