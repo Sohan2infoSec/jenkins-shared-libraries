76e13ab947f8608fa9ebd4998891c108b9aabf4d
stackops <stackops@snn-test-net-2.novalocal>
2022-01-31 11:23:22 +0700

    Rollback k8sFuncTest pass domain

--- vars/k8sFuncTestGolang.groovy
+++ vars/k8sFuncTestGolang.groovy
@@ -1,4 +1,5 @@
-def call(project, addr) {
+def call(project, domain) {
+    addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}.${domain}"
     sh "go get -d -v -t"
     sh """DURATION=1 ADDRESS=${addr} \
         go test ./... -v \
